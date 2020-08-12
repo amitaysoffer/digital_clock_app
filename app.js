2659957189b94df509808ec93b39a0eaca731693
amitaysoffer <amitaysof@gmail.com>
2020-08-12 14:53:13 +0100

    added button function for one time click- need to add multiple clicks

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const date = new Date();
 const days = ['Sunday', 'Monday', 'Thuesday', 'Wendsday', 'Thursday', 'Friday', 'Saturday'];
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-
+// Set the day
 const getDay = function () {
     let dayNum = date.getDay();
     for (let dayIndex in days) {
@@ -14,6 +14,7 @@ const getDay = function () {
     }
 }
 
+// Set the month
 const getMonth = function () {
     let monthNum = date.getMonth();
     for (let monthIndex in months) {
@@ -24,33 +25,52 @@ const getMonth = function () {
     }
 }
 
+// Set the date
 const getDate = function () {
     let monthDate = date.getDate();
     return monthDate
 }
 
+// append the date on page
+document.getElementById('date').innerHTML = `${getDay()}, ${getMonth()} ${getDate()}`;
+
+// Set the time
 const getTime = function () {
-    let dynamicDate = new Date();
+    let startDate = new Date();
 
-    let hour = dynamicDate.getHours();
-    let minute = dynamicDate.getMinutes();
-    let second = dynamicDate.getSeconds();
+    let time = startDate.toLocaleTimeString();
+    document.getElementById('time').innerHTML = `${time}`
+}
 
-    // let time = `${hour}:${minute}:${second}`;
-    // console.log(`${hour}:${minute}:${second}`);
-    // console.log(dynamicDate.toLocaleTimeString())
-    
-    let clock;
+// Start the clock and rerun it every second
+let startClock = getTime();
+let runClock = setInterval(getTime, 1000);
+
+
+
+// Click event to change clock
+document.getElementById('btn').addEventListener('click', changeClock);
+
+function changeClock() {
+    let newDate = new Date();
+    let hour = newDate.getHours();
+
+    clearInterval(runClock);
+
+    newHour = hour - 12
+    let minute = newDate.getMinutes();
+    let second = newDate.getSeconds();
+
     if (hour > 12) {
         clock = 'PM'
     } else {
         clock = 'AM'
     };
 
-    let time = dynamicDate.toLocaleTimeString();
-    document.getElementById('time').innerHTML = `${time} ${clock}`
-}
+    document.getElementById('time').innerHTML = `${newHour}:${minute}:${second} ${clock}`;
 
-getTime();
-setInterval(getTime, 1000)
-document.getElementById('date').innerHTML = `${getDay()}, ${getMonth()} ${getDate()}`;
+    // Run the new clock every second
+    setInterval(changeClock, 1000);
+};
+
+
