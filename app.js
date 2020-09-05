29394c606af824b4f75d256987a7bb0c11379f18
amitaysoffer <amitaysof@gmail.com>
2020-09-05 14:30:22 +0100

    updated to arrow functions, added military clock toggle button

--- app.js
+++ app.js
@@ -1,68 +1,48 @@
 const days = ['Sunday', 'Monday', 'Thuesday', 'Wendsday', 'Thursday', 'Friday', 'Saturday'];
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-// Get the day
-const getDay = function () {
-    let dayNum = new Date().getDay();
-    for (let dayIndex in days) {
-        if (dayNum == dayIndex) {
+let isMilitaryTime = false;
 
-            return days[dayIndex];
-        }
-    }
+// Date functions
+const getDay = (currentDate) => days[currentDate.getDay()];
+const getMonth = (currentDate) => months[currentDate.getMonth()];
+const getDate = (currentDate) => currentDate.getDate();
+
+// 12h AM/PM clock functions
+// const getTime = () => new Date().toLocaleTimeString();
+const getHourFor12hClock = (date) => {
+    let hour = getHours(date);
+    return hour >= 12 ? hour - 12 : hour
 }
+const getAmPm = () => new Date().getHours() >= 12 ? 'PM' : 'AM';
 
-// Get the month
-const getMonth = function () {
-    let monthNum = new Date().getMonth();
-    for (let monthIndex in months) {
-        if (monthNum == monthIndex) {
+// 24h military clock functions
+const getHours = (currentDate) => currentDate.getHours();
+const getMinutes = (currentDate) => currentDate.getMinutes();
+const getSeconds = (currentDate) => currentDate.getSeconds();
 
-            return months[monthIndex];
-        }
-    }
-}
-
-// Get the date
-const getDate = function () {
-    let monthDate = new Date().getDate();
-    return monthDate
-}
-
-// Get the time
-const getTime = function () {
-    // let startDate = new Date();
-    let time = new Date().toLocaleTimeString();
-    //console.log(time);
-    return time
-}
-
-const hourClock = function () {
-    let clockTime;
-    let hour = new Date().getHours()
-
-    if (hour > 12) {
-        clockTime = 'PM'
-    } else {
-        clockTime = 'AM'
-    };
-
-    return clockTime;
-}
-
-
-// document.getElementById('btn').addEventListener('click', changeClock);
-
-
-setInterval(function () {
+const displayClock = function () {
+    const date = new Date();
 
     // append date on page
-    document.getElementById('date').innerHTML = `${getDay()}, ${getMonth()} ${getDate()}`;
+    document.getElementById('date').innerHTML = `${getDay(date)}, ${getMonth(date)} ${getDate(date)}`;
     // append time on page
-    document.getElementById('time').innerHTML = `${getTime()} ${hourClock()}`
+    if (isMilitaryTime === false) {
+        document.getElementById('time').innerHTML = `${getHourFor12hClock(date)}:${getMinutes(date)}:${getSeconds(date)} ${getAmPm()}`
+    } else {
+        document.getElementById('time').innerHTML = `${getHours(date)}:${getMinutes(date)}:${getSeconds(date)}`
+    }
+}
 
-    // document.getElementById('clock-type').innerHTML = `${changeClock()}`
+setInterval(displayClock, 1000);
 
-}, 1000);
+document.getElementById('toggle-btn').addEventListener('click', function () {
+    isMilitaryTime = isMilitaryTime === false
+        ? true
+        : false
 
+    displayClock();
+});
+
+displayClock();
 
